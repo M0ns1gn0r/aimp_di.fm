1f24fc893e74182839e2d5c7d8ffaeef25b50b2e
M0ns1gn0r <rfever@gmail.com>
2015-06-30 19:10:16 +0200

    Initial commit with the plugin stub which, however, already loads.

---
+++ DI.FM/Plugin.fs
@@ -0,0 +1,11 @@
+﻿namespace DI.FM
+
+open AIMP.SDK
+
+[<AimpPlugin("DI.FM", "Roman Nikitin", "1")>]
+type Plugin() = 
+    inherit AimpPlugin()
+    override this.HasSettingDialog = false
+    override this.ShowSettingDialog(wnd) = ()
+    override this.Initialize() = ()
+    override this.Dispose() = ()
