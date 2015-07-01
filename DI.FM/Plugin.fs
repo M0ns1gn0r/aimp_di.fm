cbf62a24e5025f074f07c5549f5dd7afe913a964
M0ns1gn0r <rfever@gmail.com>
2015-07-01 19:35:53 +0200

    Add context menu items.
    * Leave only the x86 platform, because the dependent aimp_dotnet project is built for x86 only.

--- DI.FM/Plugin.fs
+++ DI.FM/Plugin.fs
@@ -1,11 +1,24 @@
 ﻿namespace DI.FM
 
 open AIMP.SDK
+open AIMP.SDK.Services.MenuManager
+open AIMP.SDK.UI.MenuItem
 
 [<AimpPlugin("DI.FM", "Roman Nikitin", "1")>]
 type Plugin() = 
     inherit AimpPlugin()
+
+    let menuType = ParentMenuType.AIMP_MENUID_PLAYER_TRAY
+    let menuItemPlus = new StandartMenuItem("DI.FM vote +");
+    let menuItemMinus = new StandartMenuItem("DI.FM vote -");
+
     override this.HasSettingDialog = false
     override this.ShowSettingDialog(wnd) = ()
-    override this.Initialize() = ()
-    override this.Dispose() = ()
+
+    override this.Initialize() =
+        this.Player.MenuManager.Add(menuType, menuItemPlus)
+        this.Player.MenuManager.Add(menuType, menuItemMinus)
+
+    override this.Dispose() =
+        this.Player.MenuManager.Delete(menuItemPlus)
+        this.Player.MenuManager.Delete(menuItemMinus)
