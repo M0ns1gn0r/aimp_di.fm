17a5212f61edbb31e384f519c2a9e54707a1bd1e
M0ns1gn0r <rfever@gmail.com>
2015-07-05 10:44:39 +0200

    Make use of Hardcodet.Wpf.TaskbarNotification library.

--- DI.FM/Plugin.fs
+++ DI.FM/Plugin.fs
@@ -1,13 +1,15 @@
 ﻿namespace DI.FM
 
 open AIMP.SDK
+open AIMP.SDK360
 open AIMP.SDK.Services.MenuManager
 open AIMP.SDK.UI.MenuItem
 
 [<AimpPlugin("DI.FM", "Roman Nikitin", "1")>]
 type Plugin() = 
     inherit AimpPlugin()
-
+     
+    let mutable taskbarIcon = null
     let menuType = ParentMenuType.AIMP_MENUID_PLAYER_TRAY
     let menuItemPlus = new StandartMenuItem("DI.FM vote +");
     let menuItemMinus = new StandartMenuItem("DI.FM vote -");
@@ -16,9 +18,21 @@ type Plugin() =
     override this.ShowSettingDialog(wnd) = ()
 
     override this.Initialize() =
-        this.Player.MenuManager.Add(menuType, menuItemPlus)
+        taskbarIcon <- Window.createTaskBarIcon ()
+        
+        // TODO: register hotkeys.
+                
+        let menuItemPlusHandler _ =
+            // TODO: use AIMP_MSG_CMD_SHOW_NOTIFICATION to show the result of vote?
+            this.AimpCore.SendMessage(AimpMessages.AimpCoreMessageType.AIMP_MSG_CMD_SHOW_NOTIFICATION, 0, "Hello world!")
+        menuItemPlus.Click.Add menuItemPlusHandler
+
+        // Register menu items.
         this.Player.MenuManager.Add(menuType, menuItemMinus)
+        this.Player.MenuManager.Add(menuType, menuItemPlus)
 
     override this.Dispose() =
-        this.Player.MenuManager.Delete(menuItemPlus)
+        taskbarIcon.Dispose()
+
         this.Player.MenuManager.Delete(menuItemMinus)
+        this.Player.MenuManager.Delete(menuItemPlus)
---
+++ DI.FM/Window.fs
@@ -0,0 +1,30 @@
+﻿module Window
+
+open System
+open System.Windows
+open Hardcodet.Wpf.TaskbarNotification
+
+let createTaskBarIcon () = 
+    let tbi = new TaskbarIcon();
+    tbi.Icon <- Drawing.SystemIcons.Hand;
+    tbi.ToolTipText <- "This is the DI.FM notification.";
+    tbi
+
+(*let win = new Window()
+win.Title <- "Say Hello"
+win.ShowInTaskbar <- false
+win.ShowActivated <- false
+win.Visibility <- Visibility.Hidden
+win.WindowStyle <- WindowStyle.None
+win.WindowStartupLocation <- WindowStartupLocation.Manual
+win.Top <- -1000.0
+win.Left <- -1000.0
+//win.Width <- 100.0
+//win.Height <- 100.0
+//win.Content <- tbi
+
+let init () = 
+    win.Show()
+
+let deinit () = 
+    win.Close()*)
