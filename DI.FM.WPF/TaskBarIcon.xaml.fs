5eeadcdff7d591bbc6e23950fd639a00c58bfd65
M0ns1gn0r <rfever@gmail.com>
2015-08-04 21:32:34 +0200

    Add ViewModel, add disabled icon, fix gitignore.
    See http://stackoverflow.com/questions/21109672/git-ignoring-icon-files-because-of-icon-rule

---
+++ DI.FM.WPF/TaskBarIcon.xaml.fs
@@ -0,0 +1,15 @@
+﻿namespace DI.FM.WPF.ViewModels
+
+type Icons = Enabled | Disabled | Error
+
+type TaskBarIconViewModel() as x =
+    inherit FSharp.ViewModule.ViewModelBase()
+
+    let resourcePrefix = "pack://application:,,,/DI.FM.WPF;component"
+    let iconSource = x.Factory.Backing(<@ x.IconSource @>, Disabled)
+
+
+    member x.IconSource
+        with get() = match iconSource.Value with
+                     | Enabled -> resourcePrefix + "/Icons/enabled.ico"
+                     | _ -> resourcePrefix + "/Icons/disabled.ico"
