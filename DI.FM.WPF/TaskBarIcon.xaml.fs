9e7f54ac0593064bcd1a20dea2664ef5b15d43ab
M0ns1gn0r <rfever@gmail.com>
2015-08-05 21:27:53 +0200

    Prototype of exchangable UIs in tray popup.

--- DI.FM.WPF/TaskBarIcon.xaml.fs
+++ DI.FM.WPF/TaskBarIcon.xaml.fs
@@ -13,3 +13,15 @@ type TaskBarIconViewModel() as x =
         with get() = match iconSource.Value with
                      | Enabled -> resourcePrefix + "/Icons/enabled.ico"
                      | _ -> resourcePrefix + "/Icons/disabled.ico"
+
+
+type LoginViewModel() as x =
+    inherit FSharp.ViewModule.ViewModelBase()
+
+    member x.Title = "This is a login page."
+
+
+type NoTrackPlaysViewModel() as x =
+    inherit FSharp.ViewModule.ViewModelBase()
+
+    member x.Title = "No DI.FM tracks are playing at the moment."
